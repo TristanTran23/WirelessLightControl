0fd1fb818d59d9174561d16f93fbf65e433e8793
TristanTran23 <tranquangthinhtdn@gmail.com>
2026-04-05 11:08:54 -0400

    set up microcontroller socket

---
+++ main.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+#include <Wifi.h>
+#include "arduino_secret.h"
+
+Servo myServo;
+const int servoPin = 2;
+WifiServer socketServer(8080);
+
+void setup() {
+  Serial.begin(115200);
+  myServo.setPeriodHertz(50);
+  myServo.attach(servoPin, 500, 2400);
+  myServo.write(POS_OFF);
+
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  socketServer.begin();
+}
+
+void loop() {
+  WiFiClient client = socketServer.available();
+  if (client) {
+    Serial.println("Connected");
--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-import socket
-import time
-
-import network
-from machine import PWM, Pin
+import cv2
+from ultralytics import YOLO
 
 
 def main():
-    servo = PWM(Pin(2), freq=50)
+    model = YOLO('./runs/detect/train5/weights/best.pt')
 
-def set_angle(angle):
-    dute = int((angle/180) * ())
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model(frame, stream=True, verbose=False)
+
+        for r in results:
+            for box in r.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                label = model.names[int(box.cls[0])]
+                conf = float(box.conf[0])
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                text = f"{label}: {conf:.2f}"
+                cv2.putText(frame, text, (x1, y1 - 10), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        cv2.imshow('YOLOv12 Hand Detection', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
